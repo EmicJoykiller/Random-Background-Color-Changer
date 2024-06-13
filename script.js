c6c5e262c5fe76f2a02f8718a95e2d9713afe48b
nikan <nikanemami7@gmail.com>
2024-06-13 11:23:32 -0400

    Update v.1.1

--- script.js
+++ script.js
@@ -25,4 +25,5 @@ const darkColorsArr = [
     bgHexCodeSpanElement.innerText = color;
     body.style.backgroundColor = color;
   }
-  changeBackgroundColor();
+  const btn = document.querySelector("#click-btn");
+  console.log(btn);
